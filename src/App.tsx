9180a5f54f7cd3c32ba81e7bd734c7c8b5d28628
Christian Santos <christian.santos@ibti.org.br>
2020-05-19 09:16:01 -0300

    criando estrutura de Toasts

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 import GlobalStyle from './styles/global';
 import SignIn from './pages/SigIn';
-import SignUp from './pages/SigUp';
+// import SignUp from './pages/SigUp';
 
+import ToastContainer from './components/ToastContainer';
 import { AuthProvider } from './context/AuthContext';
 
 const App: React.FC = () => {
@@ -12,6 +13,8 @@ const App: React.FC = () => {
       <AuthProvider>
         <SignIn />
       </AuthProvider>
+
+      <ToastContainer />
 
       <GlobalStyle />
     </>
